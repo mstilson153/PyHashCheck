8ab36147f5e5f20a1f629f17dac1f359a02f15c7
Michael Stilson <mstilson153@gmail.com>
2023-01-20 14:48:11 -0500

    Add copyright.

--- PyHashCheck.py
+++ PyHashCheck.py
@@ -1,3 +1,7 @@
+# PyHashCeck Copyright (c) Michael Stilson 2023
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License.
+
 # Imports for application.
 import os
 import sys
