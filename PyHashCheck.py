79975e8c00a6417504aae75e90e17a957860dbfd
Michael Stilson <mstilson153@gmail.com>
2023-01-20 14:53:35 -0500

    Update copyright.

--- PyHashCheck.py
+++ PyHashCheck.py
@@ -1,4 +1,4 @@
-# PyHashCeck Copyright (c) Michael Stilson 2023
+# Copyright (c) Michael Stilson 2023
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License.
 
